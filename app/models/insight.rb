0bc32d4f1690c2a198e322d35d2e6b5b2bb9fd10
Graeme Porteous <graeme@rgbp.co.uk>
2024-10-24 16:32:23 +0100

    Add Admin::InsightsController
    Allow admins to show, create, destroy insights for info requests from
    within the Admin UI.

--- app/models/insight.rb
+++ app/models/insight.rb
@@ -12,6 +12,9 @@
 #  updated_at      :datetime         not null
 #
 class Insight < ApplicationRecord
+  admin_columns exclude: [:template, :output],
+                include: [:duration, :prompt, :response]
+
   after_commit :queue, on: :create
 
   belongs_to :info_request, optional: false
@@ -33,10 +36,21 @@ class Insight < ApplicationRecord
     client.tags.first['models'].map { _1['model'] }.sort
   end
 
+  def duration
+    return unless output['total_duration']
+
+    seconds = output['total_duration'].to_f / 1_000_000_000
+    ActiveSupport::Duration.build(seconds.to_i).inspect
+  end
+
   def prompt
     template.gsub('[initial_request]') do
       outgoing_messages.first.body
     end
+  end
+
+  def response
+    output['response']
   end
 
   private
